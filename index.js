8b1c4e2f4fe329f309061db7772f3da615d58c7c
PhamSinh <sinhpv86@gmail.com>
2018-09-21 13:46:37 +0700

    update source

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = require('http').Server(app)
 
 var io = require('socket.io')(server)
 
-server.listen(3001)
+server.listen(process.env.PORT)
 app.get('/', function(req, res) {
     res.render('home');
 })
